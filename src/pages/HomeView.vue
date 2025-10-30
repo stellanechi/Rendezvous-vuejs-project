3cdc651dffe7e81f4390cc0defcb89071c936ac6
stellanechi <stellanechi65@gmail.com>
2025-10-30 15:48:37 +0100

    updated home view page to contain component alone

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -20,11 +20,5 @@ export default {
     <Banner />
     <TrendingEvents />
     <Discover />
-
-    <!-- <div class="p-10 font-semibold text-lg">
-      Selected Event:
-      {{ selectedEvent ? selectedEvent.title : "No event selected" }}
-    </div> -->
-    <!-- <TrendingEvents @event-selected="selectedEvent = $event" /> -->
   </div>
 </template>
